b2ef1cf50965d6a8bb260805048f71795f38c42b
King Joker <dungan_joshua@yahoo.com>
2021-11-04 21:30:14 -0500

    assessment

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,55 @@
 console.log('JavaScript code has loaded!')
 
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Joshua Dungan" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+let total = 0
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+document.getElementById('add-gb').addEventListener('click', function increase (gb) {
+    tbody.td(0+1)
+        console.log('Gingerbread + button was clicked!')
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+document.getElementById('minus-gb').addEventListener('click', function decrease(gb) {
+
+    console.log('Gingerbread - button was clicked!')
+
+    
+})
+
+document.getElementById('minus-cc').addEventListener('click', function(cc) {
+    
+    console.log('Chocolate Chip - button was clicked!')
+
+    
+})
+
+document.getElementById('add-cc').addEventListener('click', function(cc) {
+    
+    console.log('Chocolate Chip + button was clicked!')
+
+    
+})
+
+document.getElementById('add-sugar').addEventListener('click', function(sugar) {
+    
+    console.log('Sugar Sprinkle + button was clicked!')
+
+    
+})
+
+document.getElementById('minus-sugar').addEventListener('click',function(sugar){
+
+    
+    console.log('Sugar Sprinkle - button was clicked!')
+
+    
+})
