50d832f2f1137ffa8f91e8192a89ac8cde9da954
King Joker <dungan_joshua@yahoo.com>
2021-11-08 20:10:26 -0600

    finnished

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,113 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
-// First, tell us your name
 let yourName = "Joshua Dungan" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
-let total = 0
 
+var gbQuantity;
+var ccQuantity;
+var sugarQuantity;
+var total = 0;
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function increase (gb) {
-    tbody.td(0+1)
-        console.log('Gingerbread + button was clicked!')
+document.getElementById('add-gb').addEventListener('click', function () {
+    if(gb >= 0)
+    {
+        gb++
+    }
+    gbQuantity = document.getElementById('qty-gb');
+    gbQuantity.textContent = gb;
+    totalgbQuantity = document.getElementById('qty-total');
+    total = total + 1;
+    totalgbQuantity.textContent = total;
+    console.log('Gingerbread + button was clicked!')
 })
 
-document.getElementById('minus-gb').addEventListener('click', function decrease(gb) {
-
+document.getElementById('minus-gb').addEventListener('click', function () {
+    if(gb > 0)
+    {
+        gb--
+    
+    gbQuantity = document.getElementById('qty-gb');
+    gbQuantity.textContent = gb;
+    totalgbQuantity = document.getElementById('qty-total');
+    total = total - 1;
+    if(gb >= 0)
+    
+    totalgbQuantity.textContent = total;
+    }
     console.log('Gingerbread - button was clicked!')
-
+    
     
 })
 
-document.getElementById('minus-cc').addEventListener('click', function(cc) {
+document.getElementById('minus-cc').addEventListener('click', function() {
+    if(cc > 0)
+    {
+        cc--
     
+    ccQuantity = document.getElementById('qty-cc');
+    ccQuantity.textContent = cc;
+    totalccQuantity = document.getElementById('qty-total');
+    total = total - 1;
+    if(cc >= 0)
+    
+    totalccQuantity.textContent = total;
+    }
     console.log('Chocolate Chip - button was clicked!')
 
     
 })
 
-document.getElementById('add-cc').addEventListener('click', function(cc) {
-    
+document.getElementById('add-cc').addEventListener('click', function() {
+    if(cc >= 0)
+    {
+        cc++
+    }
+    ccQuantity = document.getElementById('qty-cc');
+    ccQuantity.textContent = cc;
+    totalccQuantity = document.getElementById('qty-total');
+    total = total + 1;
+    totalccQuantity.textContent = total;
     console.log('Chocolate Chip + button was clicked!')
 
     
 })
 
-document.getElementById('add-sugar').addEventListener('click', function(sugar) {
+document.getElementById('add-sugar').addEventListener('click', function() {
+    if(sugar >= 0)
+    {
+        sugar++
     
+    sugarQuantity = document.getElementById('qty-sugar');
+    sugarQuantity.textContent = sugar;
+    totalsugarQuantity = document.getElementById('qty-total');
+    total = total + 1;
+    if(sugar > 0)
+    
+    totalsugarQuantity.textContent = total;
+    }
     console.log('Sugar Sprinkle + button was clicked!')
 
     
 })
 
-document.getElementById('minus-sugar').addEventListener('click',function(sugar){
-
+document.getElementById('minus-sugar').addEventListener('click',function(){
+    if(sugar > 0)
+    {
+        sugar--
     
+    sugarQuantity = document.getElementById('qty-sugar');
+    sugarQuantity.textContent = sugar;
+    totalgbQuantity = document.getElementById('qty-total');
+    total = total - 1;
+    if(sugar >= 0)
+    
+    totalsugarQuantity.textContent = total;
+    }
     console.log('Sugar Sprinkle - button was clicked!')
 
     
